52bb061d568ff6e146e77bfb35758db885af656d
Berkay-Disli <berkaydisli.work@gmail.com>
2022-04-17 22:35:27 +0300

    Initial Commit

---
+++ FirebaseAuthentication/FirebaseAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthenticationApp.swift
+//  FirebaseAuthentication
+//
+//  Created by Berkay Disli on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
